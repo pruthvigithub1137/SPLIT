4a0642c4231c9476716387c1e26c65797c4683b9
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-07-05 03:36:56 +0800

    Update split.sh

--- split.sh
+++ split.sh
@@ -5,9 +5,9 @@
 #################################
 
 ETHPOOL=ethash.unmineable.com:3333
-ETHWALLET=ETH:0x6e890745813facaeab2afab23440a04f6b55034a.GPUA#z8ef-vi71
+ETHWALLET=ETH:0x6e890745813facaeab2afab23440a04f6b55034a.GPUA#wqgy-dy0w
 
-ETCPOOL=ethash.unmineable.com:3333
+ETCPOOL=ethash.unmineable.com:13333
 ETCWALLET=ETC:0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.GPUA#e34n-zbpz
 
 #################################
