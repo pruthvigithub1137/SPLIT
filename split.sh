879f1b3cf81e8e785beac5bee0f2f7839ad4cf28
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-07-05 03:37:44 +0800

    Update split.sh

--- split.sh
+++ split.sh
@@ -5,10 +5,10 @@
 #################################
 
 ETHPOOL=ethash.unmineable.com:3333
-ETHWALLET=ETH:0x6e890745813facaeab2afab23440a04f6b55034a.GPUA#wqgy-dy0w
+ETHWALLET=ETH:0x6e890745813facaeab2afab23440a04f6b55034a.GPU-1#wqgy-dy0w
 
 ETCPOOL=ethash.unmineable.com:13333
-ETCWALLET=ETC:0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.GPUA#e34n-zbpz
+ETCWALLET=ETC:0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.GPU-2#e34n-zbpz
 
 #################################
 ##  End of user-editable part  ##
