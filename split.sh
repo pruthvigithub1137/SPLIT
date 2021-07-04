2787f9d9013580b720a912da87eba4ae315d49c2
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-07-05 03:18:35 +0800

    Update split.sh

--- split.sh
+++ split.sh
@@ -16,4 +16,4 @@ ETCWALLET=0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.GPU-T4#e34n-zbpz
 
 cd "$(dirname "$0")"
 
-./lolMiner --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode etc --dualstratum $ETCWALLET@$ETCPOOL  $@
+./3 --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode etc --dualstratum $ETCWALLET@$ETCPOOL  $@
